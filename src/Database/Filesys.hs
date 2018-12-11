da351f021d14c02332e388cc26b405927abff40d
Tej Chajed <tchajed@mit.edu>
2018-12-11 10:07:29 -0500

    Don't abstract over file handles
    Just makes writing individual functions more painful, since everything
    takes an extra fh type parameter.

--- src/Database/Filesys.hs
+++ src/Database/Filesys.hs
@@ -1,21 +1,25 @@
 module Database.Filesys where
 
 import qualified Data.ByteString as BS
+import qualified System.Posix.Types
 
+-- A filename (should not contain any path separators).
 type FName = String
+-- A File is a handle to an open file.
+type File = System.Posix.Types.Fd
 
--- fh is the type of filesystem handles (System.Posix.Types.Fd or GHC.IO.Handle,
--- depending on whether we use package unix or built-in Haskell file operations)
-data Filesys fh = Filesys {
+-- Filesys is an abstraction of the filesystem exposing a single directory and
+-- only the APIs used by the database.
+data Filesys = Filesys {
     -- reading
-    open :: FName -> IO fh
+    open :: FName -> IO File
   , list :: IO [FName]
-  , size :: fh -> IO Int
+  , size :: File -> IO Int
   , readAt :: Int -> Int -> IO BS.ByteString
 
     -- modifying
-  , create :: FName -> IO fh
-  , append :: fh -> BS.ByteString -> IO ()
+  , create :: FName -> IO File
+  , append :: File -> BS.ByteString -> IO ()
   , delete :: FName -> IO ()
   , truncate :: FName -> IO ()
   , atomicCreate :: FName -> BS.ByteString -> IO ()
